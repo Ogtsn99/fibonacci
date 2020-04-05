c6ca24fd105bb3e20b69ac89a6d901d2d8afa365
Ogtsn99 <b184362@ogatakeisatorunoMacBook.local>
2020-04-05 23:36:47 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+function fib(n) {
+  if(memo.get(n) != undefined) return memo.get(n);
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+  memo.set(n, fib(n - 1) + fib(n - 2));
+  return fib(n - 1) + fib(n - 2);
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
